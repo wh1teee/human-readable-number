f04475c1035027e52575b0f6b4785d58722720ce
wh1teee <wh1teee1337lol@gmail.com>
2020-10-19 19:57:09 +0300

    feat: revork algoritm

--- src/index.js
+++ src/index.js
@@ -1,37 +1,52 @@
+let num = {
+    1: "one", 2: "two", 3: "three", 4: "four", 5: "five",
+    6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten",
+    11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen",
+    16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen", 20: "twenty",
+    30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy",
+    80: "eighty", 90: "ninety",
+}
+
 module.exports = function toReadable(number) {
     let numberArr = number.toString().split('')
-    let a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen ']
-    let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-
-    if (number == 0) {
+    if (number === 0) {
         return 'zero'
     }
+
+    if (number === 0) {
+        return zero
+    }
+
     if (number < 20) {
-        return a[number].toString().split('').slice(0, -1).join('')
+        return num[number]
+    }
+
+    if (number % 100 === 0) {
+       return num[numberArr[0]] + ' hundred'
+    }
+    if (number % 10 === 0 && number > 100) {
+      return num[numberArr[0]] + ' hundred ' + num[numberArr.splice(1).join('')]
+    }
+    if (number % 10 === 0 && number < 100) {
+       return num[number]
     }
 
     if (number < 100) {
-        if (number % 10 == 0) {
-            return b[numberArr[0]]
-        }
-        return b[numberArr[0]] + ' ' + a[numberArr[1]].toString().split('').slice(0, -1).join('')
+        return num[numberArr[0]*10] + ' ' + num[numberArr[1]]
     }
 
-    if (number < 1000) {
-        if (number % 100 == 0) {
-            return a[numberArr[0]] + 'hundred'
-        }
-        if (number % 100 == 10) {
-            return a[numberArr[0]] + 'hundred ten'
-        }
-        if (number % 10 == 0) {
-            return a[numberArr[0]] + 'hundred ' + b[numberArr[1]]
+    //if (1-9)** >= 20 (233 or 120)
+    if (number > 100 && numberArr.slice(1).join('') >= 20) {
+        return num[numberArr[0]] + ' hundred ' + num[numberArr[1]*10] + ' ' + num[numberArr[2]]
+    }
+
+    //if (1-9)** <= 20 (209  or 119)
+    if (number > 100 && numberArr.slice(1).join('') < 20) {
+        if (numberArr.slice(1).join('') < 10) {
+            return num[numberArr[0]] + ' hundred ' + num[numberArr.slice(-1).join('')]
         }
 
-if (numberArr[1] + numberArr[2] < 20) {
-    return a[numberArr[0]] + 'hundred ' + a[+(numberArr[1] + numberArr[2])].toString().split('').slice(0, -1).join('')
-}
-        return a[numberArr[0]] + 'hundred ' + b[numberArr[1]] + ' ' + a[numberArr[2]].toString().split('').slice(0, -1).join('')
+        return num[numberArr[0]] + ' hundred ' + num[numberArr.slice(1).join('')]
     }
 }
 
